b22d99bf5380c26a84977d0a874d7497e37f3826
stephen mcquay <stephen@mcquay.me>
2018-02-25 01:52:57 -0800

    Creates pgp dir if it doesn't exist

--- keyring/keyring.go
+++ keyring/keyring.go
@@ -2,12 +2,23 @@ package keyring
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/pkg/errors"
 	"golang.org/x/crypto/openpgp"
+
+	"mcquay.me/fs"
 )
 
 func NewKeyPair(root, name, email string) error {
+	pgpDir := filepath.Join(root, "var", "lib", "pm", "pgp")
+	if !fs.Exists(pgpDir) {
+		if err := os.MkdirAll(pgpDir, 0755); err != nil {
+			return errors.Wrap(err, "mk pgp dir")
+		}
+	}
+
 	e, err := openpgp.NewEntity(name, "pm", email, nil)
 	if err != nil {
 		errors.Wrap(err, "new entity")
