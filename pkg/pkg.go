6066c3bd5524d3c3d1b9a2c6113924afcaffc845
stephen mcquay <stephen@mcquay.me>
2018-02-26 20:02:23 -0800

    Simple pre-flight checks
    More of this to come ...

--- pkg/pkg.go
+++ pkg/pkg.go
@@ -1,8 +1,18 @@
 package pkg
 
-import "fmt"
+import (
+	"fmt"
+
+	"mcquay.me/fs"
+)
 
 // Create traverses the contents of dir and emits a valid pkg, signed by id
 func Create(dir, id string) error {
+	if !fs.Exists(dir) {
+		return fmt.Errorf("%q: doesn't exist", dir)
+	}
+	if !fs.IsDir(dir) {
+		return fmt.Errorf("%q: is not a directory", dir)
+	}
 	return fmt.Errorf("creating package from %q for %q: NYI", dir, id)
 }
