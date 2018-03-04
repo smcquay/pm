63c5def07f3724c813a8be75977c62c2f6dfc4c8
stephen mcquay <stephen@mcquay.me>
2018-03-03 22:24:37 -0800

    stub out fake installs of available packages

--- pkg/install.go
+++ pkg/install.go
@@ -1,10 +1,25 @@
 package pkg
 
 import (
+	"log"
+
 	"github.com/pkg/errors"
+	"mcquay.me/pm/db"
 )
 
 // Install fetches and installs pkgs from appropriate remotes.
 func Install(root string, pkgs []string) error {
+	av, err := db.LoadAvailable(root)
+	if err != nil {
+		return errors.Wrap(err, "loading available db")
+	}
+
+	ms, err := av.Installable(pkgs)
+	if err != nil {
+		return errors.Wrap(err, "checking ability to install")
+	}
+	for _, m := range ms {
+		log.Printf("fake install %v", m)
+	}
 	return errors.New("NYI")
 }
